b890634387b1dcc26cff0f683e058dbaeb8e6604
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-10-27 17:33:32 +0200

    work on home page and detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppConfigService } from './app.config.service';
+import {MatIconModule} from '@angular/material/icon';
+
+import { LightboxModule } from 'ngx-lightbox';
+
 
 import { FichedetailComponent } from './fichedetail/fichedetail.component';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +19,7 @@ import { AuthentInterceptor } from './http.interceptor.service';
 
 @NgModule({
   declarations: [AppComponent, FichedetailComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, CommonModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule, CommonModule, HttpClientModule, MatInputModule, LightboxModule],
   providers: [
     {
       provide: APP_INITIALIZER,
--- src/app/fichedetail/fichedetail.component.ts
+++ src/app/fichedetail/fichedetail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
+
 
 @Component({
   selector: 'app-fichedetail',
   templateUrl: './fichedetail.component.html',
   styleUrls: ['./fichedetail.component.css']
 })
-export class FichedetailComponent {
+export class FichedetailComponent implements OnInit {
+  constructor(private _lightBox: Lightbox){}
+  public album: Array<any> = [];
+
+  ngOnInit() {
+    window.scroll(0, 0);
+
+    for (let i = 1; i <= 8; i++) {
+      const src = "https://stjacq.jnld.xyz/download/noco/StJACQ/TestMedias/image/QHzdH5ie6VEySMq67A.jpg"
+      const img = {
+         src: src,
+      };
+
+      this.album.push(img);
+    }
+  }
+
+  open(i: number) {
+    this._lightBox.open(this.album, i);
+
+  }
+  close(): void {
+    // close lightbox programmatically
+    this._lightBox.close();
+  }
 
 }
